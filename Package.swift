8b88b3726221f96a8ef911eb711a4a07ec48a384
viachpaliy <viachpaliy@ukr.net>
2018-06-17 14:31:15 +0300

    change version

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ let package = Package(
     name: "Gdk",
     dependencies: [
         .Package(url: "https://github.com/viachpaliy/CGdk.git", majorVersion: 1),
-        .Package(url: "https://github.com/viachpaliy/SwiftGdkPixbuf.git", majorVersion: 2),
-        .Package(url: "https://github.com/viachpaliy/SwiftPangoCairo.git", majorVersion: 2)
+        .Package(url: "https://github.com/viachpaliy/SwiftGdkPixbuf.git",  majorVersion: 1),
+        .Package(url: "https://github.com/viachpaliy/SwiftPangoCairo.git", majorVersion: 1)
     ],
     swiftLanguageVersions: [3, 4]
 )
